eb31a33a367f822e2552b095761529f04531e965
PraisesPJMT <praisesmusa@outlook.com>
2022-07-12 23:32:52 +0000

    Fixed linter errors on _putchar.c function

--- _putchar.c
+++ _putchar.c
@@ -3,10 +3,10 @@
 /**
  * _putchar - Prints an ASCII character
  * @c: Character to be printed
- * 
+ *
  * Return: Zero(0) upon success
  */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }

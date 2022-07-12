b7cf60c17b500fb14dff90ef937420dc1e5d0da8
Idowu-David <idowudavidodun@gmail.com>
2022-07-13 00:26:13 +0100

    Modified main.h, added validate_format function

--- _printf.c
+++ _printf.c
@@ -30,3 +30,52 @@ int _printf(const char *format, ...)
 
 	return (printCount);
 }
+
+/**
+ * validate_format - Function that produces output according to a format.
+ * @format: Pointer
+ * @arg: va_list
+ * @specStruct: struct
+ * Return: Always 0
+ */
+int validate_format(const char *format, va_list arg, formatType *specStruct)
+{
+        int indexA = 0, indexB = 0, prtCount = 0;
+
+        while (format && format[indexA])
+        {
+                if (format[indexA] == '%' && format[indexA + 1] == '\0')
+                        return (-1);
+                if (format[indexA] == '%' && (format[indexA + 1] == ' '
+                         || format[indexA + 1] != '%'))
+                {
+                        if (format[indexA + 1] == ' ')
+                        {
+                                while (format[indexA + 1] == ' ')
+                                indexA++;
+                        }
+                        while (indexB < 7)
+                        {
+                                if (specStruct[indexB].specifier[0] == format[indexA + 1])
+                                {
+                                        prtCount += specStruct[indexB].print_func(arg);
+                                        indexA++;
+                                        break;
+                                } indexB++;
+                        }
+                        if (indexB == 6)
+                                prtCount += _putchar('%');
+                }
+                else if (format[indexA] == '%' && format[indexA + 1] == '%')
+                {
+                        prtCount += _putchar('%');
+                        indexA++;
+                }
+                else
+                {
+                        prtCount += _putchar(format[indexA]);
+                }
+                indexA++, indexB = 0;
+        }
+        return (prtCount);
+}                                                                                                                                      
--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
 /* All Standard Libraries */
 #include <stdio.h>
 #include <unistd.h>
@@ -8,5 +7,28 @@
 #include <limits.h>
 #include <stdlib.h>
 
+/* Type Definition of Structure */
+/**
+ * struct format - Data type of a structured format
+ * @specifier: Specifier formats
+ * @print_func: Print functions
+ */
+
+typedef struct format
+{
+        char *specifier;
+        int (*print_func)(va_list arg);
+} formatType;
+
 /* General Functions */
+int _putchar(char c);
 int _printf(const char *format, ...);
+
+/* General Functions */
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+/* Other Functions */
+int validate_format(const char *format, va_list arg, formatType *specStruct);
+
+#endif

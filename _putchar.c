8dc233f78f60c247de7e8f9c23e7e5c5c25b6839
PraisesPJMT <praisesmusa@outlook.com>
2022-07-12 23:28:30 +0000

    Added _putchar.c function

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - Prints an ASCII character
+ * @c: Character to be printed
+ * 
+ * Return: Zero(0) upon success
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
